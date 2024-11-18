fdc99e5e3f2722afd8863dc8979919b0e99d3c0c
lontongyt <jaydenharianja@gmail.com>
2024-11-18 22:22:05 +0700

    Update Blair Script [KAVO UI].lua

--- Blair Script [KAVO UI].lua
+++ Blair Script [KAVO UI].lua
@@ -1,7 +1,35 @@
 --workspace.Map.ChildAdded:Connect(GhAdd)
+local BFunc = loadstring(game:HttpGet("https://raw.githubusercontent.com/lontongyt/Toolong/refs/heads/main/Blair%20Module%20INDEV.lua"))()
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+evles1,evles2,evAct = BFunc.CheckChallenges()
+--NOTIF SYSTEM
+function NotifyPlr(NotifType,TitleS,Desc,Color,TimeS,ImgClr,Img)
+if NotifType == "option" then
+Notification:Notify(
+    {Title = TitleS, Description = Desc},
+    {OutlineColor = Color,Time = TimeS, Type = "option"},
+    {Image = Img, ImageColor = ImgClr, Callback = function(State) print(tostring(State)) end}
+)
+elseif NotifType == "image" then
+Notification:Notify(
+    {Title = TitleS, Description = Desc},
+    {OutlineColor = Color,Time = TimeS, Type = "image"},
+    {Image = Img, ImageColor = ImgClr}
+)
+else
+Notification:Notify(
+    {Title = TitleS, Description = Desc},
+    {OutlineColor = Color,Time = TimeS, Type = "default"}
+)
+end
+end
+--NOTIF SYSTEM END
 
 
-
+ 
 
 
 local Window = Library.CreateLib("Blair Script [KAVO UI EDITION]", "Synapse")
@@ -15,31 +43,26 @@ local toggle = EvSec:NewToggle("Ghost ESP", "Ghost ESP", function(state)
 end)
 
 EvSec:NewButton("Check Temperature", "check temp all acros the building", function()
-                    task.spawn(GetTemp)
+                    strgPrt = task.spawn(BFunc.GetTemp)
 		    NotifyPlr("","Evidence Check","Detecting temperature",Color3.new(1,1,0),2.25,"","")
                     task.wait(2)
 		    NotifyPlr("","Evidence Check",strgPrt,Color3.fromRGB(173, 216, 230),4.5,"","")
                     --getgenv().Kavo:Notify(strgPrt, 5)
 	end)
 EvSec:NewButton("Check UV", "check UV all acros the building", function()
+	BFunc.FindUVAlt()
 	NotifyPlr("","Evidence Check",uvsent..evles1..evles2..evAct,Color3.new(0,1,0),3,"","")
 	--getgenv().Kavo:Notify(uvsent..evles1..evles2..evAct, 5) -- getgenv, getrenv, getsenv, getfenv
 	end)
 EvSec:NewButton("Check ORB", "check ORB all acros the building", function()
-                    FindOrbAlt()
+                    BFunc.FindOrbAlt()
 		NotifyPlr("","Evidence Check",orbSent..evles1..evles2..evAct,Color3.new(0,1,0),3,"","")
 		--getgenv().Kavo:Notify(orbSent..evles1..evles2..evAct, 5) -- getgenv, getrenv, getsenv, getfenv
 	end)
 
 game:GetService("RunService").RenderStepped:Connect(function()
 	if getgenv().Toggled then
-		GESP = getgenv().Toggled
-                        GhostHL = workspace.Ghost
-                        task.spawn(chkspd,GhostHL)
-                        GhostHL.Highlight.Enabled = true
-		         NotifyPlr("","Speed",tostring(GhostHL.Humanoid.WalkSpeed),Color3.new(1,0,0),0.8,"","")
-                        -- getgenv().Kavo:Notify(tostring(GhostHL.Humanoid.WalkSpeed),1)
+			
 	else
-		GESP = getgenv().Toggled
 	end
 end)
